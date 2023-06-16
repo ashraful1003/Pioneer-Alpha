9b62ee55ede478209345bbf33e577151b9a9f9dd
Ashraful Alam <alamashraful1003@gmail.com>
2023-06-16 14:11:12 +0600

    Some dependencies added

---
+++ lib/controllers/init_controller.dart
@@ -0,0 +1,8 @@
+import 'package:get/get.dart';
+
+class InitController implements Bindings {
+  @override
+  void dependencies() {
+    // TODO: implement dependencies
+  }
+}
---
+++ lib/models/products_model.dart
@@ -0,0 +1,41 @@
+import 'dart:convert';
+
+List<ProductsModel> productsModelFromJson(String str) =>
+    List<ProductsModel>.from(
+        json.decode(str).map((x) => ProductsModel.fromJson(x)));
+
+String productsModelToJson(List<ProductsModel> data) =>
+    json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
+
+class ProductsModel {
+  int id;
+  String title, description, category, image;
+  double price;
+
+  ProductsModel({
+    required this.id,
+    required this.title,
+    required this.price,
+    required this.description,
+    required this.category,
+    required this.image,
+  });
+
+  factory ProductsModel.fromJson(Map<String, dynamic> json) => ProductsModel(
+        id: json["id"],
+        title: json["title"],
+        price: json["price"].toDouble(),
+        description: json["description"],
+        category: json["category"],
+        image: json["image"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "id": id,
+        "title": title,
+        "price": price,
+        "description": description,
+        "category": category,
+        "image": image,
+      };
+}
